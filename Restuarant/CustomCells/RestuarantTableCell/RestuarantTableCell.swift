38de86ada3960f9fd46806cd0515ecfe8cbb88c5
Faran <faranmushtaq16@gmail.com>
2019-03-23 14:17:19 +0500

    first commit

---
+++ Restuarant/CustomCells/RestuarantTableCell/RestuarantTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestuarantTableCell.swift
+//  Restuarant
+//
+//  Created by Faran on 3/23/19.
+//  Copyright © 2019 Faran. All rights reserved.
+//
+
+import UIKit
+
+class RestuarantTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
